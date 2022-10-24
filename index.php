bec10f86e8a75ffbf0fa02eb9e99090655da2f60
bagusmaulanairkham <bagusmaulanairkham@gmail.com>
2022-10-24 11:41:32 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello word<br>";
+
+$nama = "Bagus";
+$umur = 20;
+
+echo "Nama saya <strong> $nama</strong>, saya berusia  $umur tahun <br>";
+
+$namaAdik = "Hima";
+$umurAdik = 13;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun<br>";
+
+$selisihUmur = $umur-$umurAdik;
+
+echo "Selisih umur saya dan adik saya $selisihUmur tahun <br/>";
+
+
+?>
